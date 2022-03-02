6450dc98a43a1cd7b7fe78e56df3bf702728ba3b
Joseph Young <joe.southwood.young@gmail.com>
2022-03-02 16:37:07 -0500

    pre merge

--- src/client/components/FormModal.jsx
+++ src/client/components/FormModal.jsx
@@ -28,81 +28,84 @@ const FormModal = ({ formClass, handleHide }) => {
   console.log(formClass);
   return (
     <div className={formClass}>
-      <form className="form" action="/auth/getUserData" method="post" >
-        {/* mx: left/right margins; mb: bottom margin */}
-        <div className="flex flex-wrap -mx-3 mb-6">
-          <div className="w-full md:w-1/3 px-3 mb-6 md:mb-0">
-            <label className="label">completed</label>
-            <div className="relative">
-              <select className="select" id="completed" onChange={(e) => { setCompleted(e.target.value) }}>
-                <option>yes</option>
-                <option>in progress</option>
-              </select>
-            </div>
-
+      <div className="innerFormModal">
+        <h2> How did you do?</h2>
+        <form className="form" action="/auth/getUserData" method="post" >
+          {/* mx: left/right margins; mb: bottom margin */}
+          <div className="flex flex-wrap -mx-3 mb-6">
             <div className="w-full md:w-1/3 px-3 mb-6 md:mb-0">
-              <label className="label">time complexity</label>
+              <label className="label">completed</label>
               <div className="relative">
-                <select className="select" id="timeComplexity" onChange={(e) => { setTimeComp(e.target.value) }}>
-                  <option>constant: O(1)</option>
-                  <option>linear: O(n)</option>
-                  <option>quadratic: O(n<sup>2</sup>)</option>
-                  <option>logarithmic: O(log n)</option>
-                  <option>linearithmic: O(n<sup>*</sup>log n)</option>
-                </select>
-              </div>
-            </div>
-
-            <div className="w-full md:w-1/3 px-3 mb-6 md:mb-0">
-              <label className="label">difficulty</label>
-              <div className="relative">
-                <select className="select" id="difficulty" onChange={(e) => { setDifficulty(e.target.value) }}>
-                  <option>easy</option>
-                  <option>somewhat easy</option>
-                  <option>somewhat difficult</option>
-                  <option>difficult</option>
-                  <option>extremely difficult</option>
-                </select>
-              </div>
-            </div>
-          </div>
-        </div>
-
-        <div className="flex flex-wrap -mx-3 mb-2">
-          <div className="w-full md:w-1/2 px-3 mb-6 md:mb-0">
-            <label className="label" for="repeat">
-              would you like this to repeat again?
-            </label>
-            <div className="relative">
-                <select className="select" id="repeat" onChange={(e) => { setRepeat(e.target.value) }}>
+                <select className="select" id="completed" onChange={(e) => { setCompleted(e.target.value) }}>
                   <option>yes</option>
-                  <option>no</option>
+                  <option>in progress</option>
                 </select>
               </div>
 
-            <div className="w-full md:w-1/2 px-3 mb-6 md:mb-0">
-              <div className="w-full px-3">
-                <label className="label" for="notes">
-                  notes
-                </label>
-                <textarea className="textarea" id="notes" rows="3" placeholder="Notes..."></textarea>
+              <div className="w-full md:w-1/3 px-3 mb-6 md:mb-0">
+                <label className="label">time complexity</label>
+                <div className="relative">
+                  <select className="select" id="timeComplexity" onChange={(e) => { setTimeComp(e.target.value) }}>
+                    <option>constant: O(1)</option>
+                    <option>linear: O(n)</option>
+                    <option>quadratic: O(n<sup>2</sup>)</option>
+                    <option>logarithmic: O(log n)</option>
+                    <option>linearithmic: O(n<sup>*</sup>log n)</option>
+                  </select>
+                </div>
+              </div>
+
+              <div className="w-full md:w-1/3 px-3 mb-6 md:mb-0">
+                <label className="label">difficulty</label>
+                <div className="relative">
+                  <select className="select" id="difficulty" onChange={(e) => { setDifficulty(e.target.value) }}>
+                    <option>easy</option>
+                    <option>somewhat easy</option>
+                    <option>somewhat difficult</option>
+                    <option>difficult</option>
+                    <option>extremely difficult</option>
+                  </select>
+                </div>
               </div>
             </div>
           </div>
-        </div>
 
-        <div class="md:flex md:items-center">
-          <div class="md:w-1/3"></div>
-          <div class="md:w-2/3">
-            <button class="shadow bg-purple-500 hover:bg-purple-400 focus:shadow-outline focus:outline-none text-white font-bold py-2 px-4 rounded" type="button"
-              onClick={ handleSubmit}>
-              submit
-            </button>
+          <div className="flex flex-wrap -mx-3 mb-2">
+            <div className="w-full md:w-1/2 px-3 mb-6 md:mb-0">
+              <label className="label" for="repeat">
+                would you like this to repeat again?
+              </label>
+              <div className="relative">
+                  <select className="select" id="repeat" onChange={(e) => { setRepeat(e.target.value) }}>
+                    <option>yes</option>
+                    <option>no</option>
+                  </select>
+                </div>
+
+              <div className="w-full md:w-1/2 px-3 mb-6 md:mb-0">
+                <div className="w-full px-3">
+                  <label className="label" for="notes">
+                    notes
+                  </label>
+                  <textarea className="textarea" id="notes" rows="3" placeholder="Notes..."></textarea>
+                </div>
+              </div>
+            </div>
           </div>
-        </div>
-      </form>
 
-      <button className="hideButton" type="button" onClick={handleHide}> Hide </button>
+          <div className="md:flex md:items-center">
+            <div className="md:w-1/3"></div>
+            <div className="md:w-2/3">
+              <button className="shadow bg-purple-500 hover:bg-purple-400 focus:shadow-outline focus:outline-none text-white font-bold py-2 px-4 rounded" type="button"
+                onClick={ handleSubmit}>
+                submit
+              </button>
+            </div>
+          </div>
+        </form>
+
+        <button className="hideButton" type="button" onClick={handleHide}> Hide </button>
+      </div>
     </div>
   )
 }
